226ad5cb2304de0cf8dae56e06cfc6d4d221caad
jkramer26 <jkramer26@L11716042779.wctc.msft>
2014-08-26 16:54:47 -0500

    Modified main method

--- src/firstget/FirstGet.java
+++ src/firstget/FirstGet.java
@@ -17,7 +17,8 @@ public class FirstGet {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.out.println("");
+         
     }
     
 }
